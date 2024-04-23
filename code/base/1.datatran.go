c388f04478d701d2efcb4d04b499aa80d8705441
ameamezhou <wxz17352825505@163.com>
2024-04-23 23:02:16 +0800

    context 1

---
+++ code/base/1.datatran.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+type UserInfo struct {
+	Name 	string
+	Age 	int
+}
+
+func GetUser(ctx context.Context){
+	fmt.Println(ctx.Value("info").(UserInfo).Name) // 可以使用断言转化类型的
+}
+
+func main(){
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, "info", UserInfo{Name: "xiaoqizhou", Age: 18})
+	GetUser(ctx)
+}
